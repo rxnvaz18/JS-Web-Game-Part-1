d08208142a97e801929e772eddf4d1423187f0f1
rxnvaz18 <54378927+rxnvaz18@users.noreply.github.com>
2023-09-19 19:48:09 -0700

    added pine tree

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let pine = document.createElement('img');
+pine.src = 'assets/pine-tree.png';
+pine.style.position = 'fixed';
+pine.style.left = '450px';
+pine.style.bottom = '200px';
+document.body.append(pine);
